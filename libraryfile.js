c26e36e0c69d74f5d4acc189f7c57343dfad77fd
waytogithub <anshuman@fusioncharts.com>
2016-07-14 10:24:30 +0530

    Update libraryfile.js

--- libraryfile.js
+++ libraryfile.js
@@ -18,3 +18,70 @@ this.calculateMaxMin =function(obj){
         }
          return maxminobj;  //returning max and min stored in this object
 };
+this.createlimits= function(ul,ll){
+                  var ullength = ul.length;
+                  var lllength = ll.length;
+                  var cnt; 
+                  
+                  
+                  if(ul[0]==0){
+                    cnt=0;
+                    for(var i=0;i<ullength;i++){
+                        if(ul[i]==0){
+                            cnt++;
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    cnt++;
+                    if(ul[0]==9){
+                        ul=1;
+                    }
+                    else{
+                        ul = ul.replace(/^0+/, '');
+                        ul = ul.substr(0,1);
+                        ul = (parseInt(ul)+1).toString();
+                        ul = this.placezeros(ul,cnt,0);
+                    }
+                  }
+                  else{
+                    ul = ul.substr(0,1);
+                    ul = (parseInt(ul)+1).toString();
+                    ul = this.placezeros(ul,ullength,1);
+                  }
+                  if(ll[0]==0){
+                    cnt=0;
+                    for(var i=0;i<lllength;i++){
+                        if(ll[i]==0){
+                            cnt++;
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    cnt++;
+                    ll = ll.replace(/^0+/, '');
+                    ll = ll.substr(0,1);
+                    ll = (parseInt(ll)+1).toString();
+                    ll = this.placezeros(ll,cnt,0);
+                  }else{
+                    ll = ll.substr(0,1);
+                    ll = this.placezeros(ll,lllength,1);
+                  }
+                   return [ul,ll]; 
+};
+
+this.placezeros= function(data,range,method){
+        if(method==0){
+            while (data.length < range) {
+             data = '0'+data;
+            }
+            return "0."+data;
+        }else{
+            while (data.length < range) {
+             data =  data+'0';
+            }
+            return data;
+        }
+};
