1db6611f8d285cce860bd07f530c053235355d14
waytogithub <anshuman@fusioncharts.com>
2016-07-15 14:34:15 +0530

    Update jsonParsing.js

--- jsonParsing.js
+++ jsonParsing.js
@@ -4,13 +4,13 @@
   <title>JavaScript Get Json</title>
 </head>
 <body>
+  <div id="chart-container"> render here</div> 
   <script type="text/javascript">
   var xmlhttp= new XMLHttpRequest(),json;
   xmlhttp.onreadystatechange = function(){
   	if ( xmlhttp.readyState === 4 && xmlhttp.status === 200 ) {
   		
-  		json = JSON.parse(xmlhttp.responseText);
-  		console.log(json); 
+  	 renderChart("chart-container,'|',"500","500",JSON.parse(xmlhttp.responseText)); 
   	}
   };
   xmlhttp.open('GET','url',true);
